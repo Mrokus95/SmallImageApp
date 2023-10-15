09fb97c1d4d29b0959cbf39adb9dcc7ce1427906
Lukasz Mroczkowski <luk.mroczkowski@gmail.com>
2023-10-15 10:47:03 +0200

    Finished viewset tests

--- images_rest_api/tests/viewset_tests.py
+++ images_rest_api/tests/viewset_tests.py
@@ -1,15 +1,18 @@
+from unittest.mock import MagicMock
+
 import pytest
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ObjectDoesNotExist
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.urls import reverse
 from knox.auth import AuthToken
 from rest_framework import status
 from rest_framework.test import APIClient
+
 from ..models import UserImage
 from ..serializers import UserSerializer
-from .factories import CustomUserFactory, UserImageFactory
-from django.core.files.uploadedfile import SimpleUploadedFile
-
+from .factories import AccountTypeFactory, CustomUserFactory, UserImageFactory
 
 db = get_user_model()
 
@@ -32,7 +35,6 @@ class TestCreateUserView:
         non_admin_user.save()
         _, token_instance = AuthToken.objects.create(non_admin_user)
         return (non_admin_user, token_instance)
-    
 
     def test_create_user_view_user_without_permissions(self, non_admin_user):
         token_instance = non_admin_user[1]
@@ -55,7 +57,6 @@ class TestCreateUserView:
         assert db.objects.count() == 1
         with pytest.raises(ObjectDoesNotExist):
             user = db.objects.get(username="testuser")
-
 
     def test_create_user_view_success(self, user):
         token_instance = user[1]
@@ -165,7 +166,6 @@ class TestCreateUserView:
 
 
 class TestChangePasswordView:
-    
     @pytest.fixture
     def user(self):
         user = CustomUserFactory()
@@ -175,7 +175,6 @@ class TestChangePasswordView:
         return (user, token_instance)
 
     def test_change_password(self, user):
-
         user, token_instance = user
 
         url = reverse("change_password")
@@ -211,8 +210,11 @@ class TestChangePasswordView:
         initial_password = user.password
 
         response = client.patch(url, data, format="json")
-        assert response.status_code == status.HTTP_400_BAD_REQUEST 
-        assert "Unable to authenticate with provided credentials." in response.data["old_password"] # 
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert (
+            "Unable to authenticate with provided credentials."
+            in response.data["old_password"]
+        )  #
         user.refresh_from_db()
         assert user.password == initial_password
 
@@ -233,12 +235,14 @@ class TestChangePasswordView:
 
         response = client.patch(url, data, format="json")
         assert response.status_code == status.HTTP_400_BAD_REQUEST
-        assert "New password must be at least 7 characters long." in response.data["new_password"]
+        assert (
+            "New password must be at least 7 characters long."
+            in response.data["new_password"]
+        )
         user.refresh_from_db()
         assert user.password == initial_password
 
     def test_change_password_unauthorized_user(self, user):
-
         user, token_instance = user
 
         url = reverse("change_password")
@@ -258,8 +262,8 @@ class TestChangePasswordView:
         user.refresh_from_db()
         assert user.password == initial_password
 
-class TestLoginView:
 
+class TestLoginView:
     @pytest.fixture
     def user(self):
         user = CustomUserFactory()
@@ -294,8 +298,8 @@ class TestLoginView:
         response = client.post(url, data, format="json")
         assert response.status_code == status.HTTP_400_BAD_REQUEST
 
+
 class TestManageUserView:
-    
     @pytest.fixture
     def user(self):
         user = CustomUserFactory()
@@ -323,8 +327,8 @@ class TestManageUserView:
 
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
-class TestUserImagesViewSet:
 
+class TestUserImagesViewSet:
     @pytest.fixture
     def user(self):
         user = CustomUserFactory()
@@ -333,11 +337,10 @@ class TestUserImagesViewSet:
         _, token_instance = AuthToken.objects.create(user)
         UserImageFactory(author=user)
         return (user, token_instance)
-    
+
     @pytest.fixture
     def image(self, user):
         return UserImageFactory.create_image("test_image.jpg", 300)
-
 
     def test_get_user_images(self, user):
         user, token = user
@@ -351,7 +354,7 @@ class TestUserImagesViewSet:
     def test_create_user_image(self, user, image):
         user, token = user
         url = reverse("userimage-list")
-        
+
         data = {
             "name": "test_picture",
             "image": image,
@@ -360,7 +363,6 @@ class TestUserImagesViewSet:
         client.credentials(HTTP_AUTHORIZATION="Token " + token)
         response = client.post(url, data, format="multipart")
         assert response.status_code == status.HTTP_201_CREATED
-
 
     def test_delete_user_image(self, user):
         user, token = user
@@ -374,7 +376,7 @@ class TestUserImagesViewSet:
 
     def test_delete_non_existent_user_image(self, user):
         user, token = user
-        image_id = UserImage.objects.count()+1000
+        image_id = UserImage.objects.count() + 1000
         url = reverse("userimage-detail", args=[image_id])
         client = APIClient()
         client.credentials(HTTP_AUTHORIZATION="Token " + token)
@@ -395,7 +397,7 @@ class TestUserImagesViewSet:
     def test_send_not_an_image(self, user, image):
         user, _ = user
         url = reverse("userimage-list")
-        
+
         data = {
             "name": "test_picture",
             "image": image,
@@ -410,17 +412,19 @@ class TestUserImagesViewSet:
         url = reverse("userimage-list")
         client = APIClient()
         client.credentials(HTTP_AUTHORIZATION="Token " + token)
-        
-        # Tworzymy plik, który nie jest obrazem (np. plik tekstowy)
+
         not_an_image_file = SimpleUploadedFile("not_an_image.txt", b"Test file content")
-        
+
         data = {
             "name": "test_picture",
             "image": not_an_image_file,
         }
-        
+
         response = client.post(url, data, format="multipart")
-        assert "Upload a valid image. The file you uploaded was either not an image or a corrupted image." in response.data["image"]
+        assert (
+            "Upload a valid image. The file you uploaded was either not an image or a corrupted image."
+            in response.data["image"]
+        )
         assert response.status_code == status.HTTP_400_BAD_REQUEST
 
     def test_send_image_in_wrong_format(self, user):
@@ -428,16 +432,21 @@ class TestUserImagesViewSet:
         url = reverse("userimage-list")
         client = APIClient()
         client.credentials(HTTP_AUTHORIZATION="Token " + token)
-        
-        wrong_format_image = UserImageFactory.create_image("wrong_format.gif", 300, content_type="image/gif", format="GIF")
-        
+
+        wrong_format_image = UserImageFactory.create_image(
+            "wrong_format.gif", 300, content_type="image/gif", format="GIF"
+        )
+
         data = {
             "name": "test_picture",
             "image": wrong_format_image,
         }
-        
+
         response = client.post(url, data, format="multipart")
-        assert "gif - Invalid file extension. Only ['jpg', 'jpeg', 'png'] files are accepted." in response.data["image"]
+        assert (
+            "gif - Invalid file extension. Only ['jpg', 'jpeg', 'png'] files are accepted."
+            in response.data["image"]
+        )
         assert response.status_code == status.HTTP_400_BAD_REQUEST
 
     def test_send_image_in_wrong_format_but_valid_name(self, user):
@@ -446,13 +455,80 @@ class TestUserImagesViewSet:
         client = APIClient()
         client.credentials(HTTP_AUTHORIZATION="Token " + token)
 
-        wrong_format_image = UserImageFactory.create_image("wrong_format.jpg", 300, content_type="image/gif", format="GIF")
-        
+        wrong_format_image = UserImageFactory.create_image(
+            "wrong_format.jpg", 300, content_type="image/gif", format="GIF"
+        )
+
         data = {
             "name": "test_picture",
             "image": wrong_format_image,
         }
-        
+
         response = client.post(url, data, format="multipart")
-        assert "Cannot open the file as an image. Please check if the uploaded file is a valid image." in response.data["image"]
+        assert (
+            "Cannot open the file as an image. Please check if the uploaded file is a valid image."
+            in response.data["image"]
+        )
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+@pytest.mark.django_db
+class TestGenerateTemporaryLinkView:
+    @pytest.fixture
+    def user(self, mocker):
+        account_type = AccountTypeFactory(time_limited_link=True)
+        user = CustomUserFactory(account_type=account_type)
+        user.set_password("Strongpassword")
+        user.save()
+        _, token_instance = AuthToken.objects.create(user)
+        mocker.patch(
+            "knox.models.AuthToken.objects.create", return_value=token_instance
+        )
+        UserImageFactory(author=user)
+        return user, token_instance
+
+    def test_generate_temporary_link_for_user_image(self, mocker, user):
+        user, token = user
+        user_image = UserImageFactory(author=user)
+        mock_s3 = mocker.patch("boto3.client")
+        mock_generate_presigned_url = MagicMock()
+        mock_s3.return_value.generate_presigned_url = mock_generate_presigned_url
+        mock_generate_presigned_url.return_value = "temporary_url"
+
+        url = reverse(
+            "generate-temporary-link",
+            args=("user_image", user_image.id),
+        )
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION="Token " + token)
+        response = client.get(url, format="json")
+        assert response.status_code == status.HTTP_200_OK
+
+        mock_generate_presigned_url.assert_called_with(
+            "get_object",
+            Params={
+                "Bucket": settings.AWS_STORAGE_BUCKET_NAME,
+                "Key": user_image.image.name,
+            },
+            ExpiresIn=3600,
+        )
+
+    def test_generate_temporary_link_invalid_file_type(self, user):
+        user, token = user
+        url = reverse("generate-temporary-link", args=("invalid_type", 1))
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION="Token " + token)
+        response = client.get(url, format="json")
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+    def test_generate_temporary_link_invalid_expiration_time(self, user):
+        user, token = user
+        url = reverse("generate-temporary-link", args=("user_image", 1))
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION="Token " + token)
+
+        response = client.get(url, {"expiration_time_seconds": 299}, format="json")
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+        response = client.get(url, {"expiration_time_seconds": 30001}, format="json")
         assert response.status_code == status.HTTP_400_BAD_REQUEST
